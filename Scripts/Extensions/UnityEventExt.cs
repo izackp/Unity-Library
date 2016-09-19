45465488063ca935b5f6c055f9f6d5eafbd0df54
Isaac Paul <izackp@gmail.com>
2016-09-19 10:01:45 -0400

    Added Extensions Method for UnityEvent

---
+++ Scripts/Extensions/UnityEventExt.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnityEngine.Events {
+    public static class UnityEventExt {
+        public static void AddListenerWeak(this UnityEvent obj, Action call) {
+            obj.AddListener(new WeakAction(call));
+        }
+    }
+}
